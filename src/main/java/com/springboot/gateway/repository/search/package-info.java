2d14a9e1e6460de6e5fce2db4cac62c94838d2e2
江彦 <mrjiangyan@hotmail.com>
2017-02-10 23:26:20 +0800

    清空无效的文件

--- src/main/java/com/springboot/gateway/repository/search/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Data Elasticsearch repositories.
- */
-package com.springboot.gateway.repository.search;
--- src/main/java/com/springboot/gateway/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.springboot.gateway.web.rest;
